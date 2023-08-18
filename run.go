d95074832ae76fdf0882feb41199ea610f0acdbf
AspieSoft <aspiesoftweb@gmail.com>
2023-08-18 12:18:53 -0400

    updated

--- run.go
+++ run.go
@@ -41,35 +41,70 @@ func main(){
 			file = bytes.ReplaceAll(file, []byte("<Insert Cloudflare API Token>"), key)
 		}
 	}
-
+	
 	if bytes.Contains(file, []byte("<Insert Zone ID>")) {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Printf("Enter Zone ID: ")
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err)
+		if zoneListFile, err := os.ReadFile("zone.list"); err == nil {
+			defer os.WriteFile("cloudflare.tf", oldFile, 0644)
+
+			curFile := file
+
+			zoneList := bytes.Split(zoneListFile, []byte{'\n'})
+			for _, zone := range zoneList {
+				zone = bytes.TrimRight(zone, "\r")
+				if len(zone) == 0 {
+					break
+				}
+
+				file = bytes.ReplaceAll(file, []byte("<Insert Zone ID>"), []byte(zone))
+				os.WriteFile("cloudflare.tf", file, 0644)
+
+				run(true)
+
+				file = curFile
+				resetTerraform()
+			}
+
+			os.WriteFile("cloudflare.tf", oldFile, 0644)
+			resetTerraform()
+			return
+		}else{
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Printf("Enter Zone ID: ")
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+	
+			text = strings.TrimRight(text, "\r\n")
+	
+			if text == "" {
+				panic("error: Zone ID Not Specified")
+			}
+
+			hasModifiedFile = true
+			file = bytes.ReplaceAll(file, []byte("<Insert Zone ID>"), []byte(text))
 		}
-
-		text = strings.TrimRight(text, "\r\n")
-
-		if text == "" {
-			panic("error: Zone ID Not Specified")
-		}
-
-		hasModifiedFile = true
-		file = bytes.ReplaceAll(file, []byte("<Insert Zone ID>"), []byte(text))
 	}
 
 	if hasModifiedFile {
-		os.WriteFile("cloudflare.tf", file, 0644)
 		defer os.WriteFile("cloudflare.tf", oldFile, 0644)
+		os.WriteFile("cloudflare.tf", file, 0644)
 	}
 
+	run(autoYes)
+
+	if hasModifiedFile {
+		os.WriteFile("cloudflare.tf", oldFile, 0644)
+		resetTerraform()
+	}
+}
+
+func run(autoYes bool){
 	cmd := exec.Command(`terraform`, `init`)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	err := cmd.Run()
 
 	if err == nil {
 		cmd = exec.Command(`terraform`, `plan`)
@@ -90,11 +125,6 @@ func main(){
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
-	}
-
-	if hasModifiedFile {
-		os.WriteFile("cloudflare.tf", oldFile, 0644)
-		resetTerraform()
 	}
 }
 
