7db2baba0932ad8173635fca3f2a571e7403b890
AspieSoft <aspiesoftweb@gmail.com>
2023-08-30 13:04:52 -0400

    updated

--- src/buid.sh
+++ src/buid.sh
@@ -2,6 +2,7 @@
 
 cd $(dirname "$0")
 
-go build -o ../run
+CGO_ENABLED=0 go build -o ../run
 
-GOOS=windows go build -o ../run.exe
+CGO_ENABLED=0 GOOS="windows" GOARCH="amd64" go build -o ../run.amd64.exe
+CGO_ENABLED=0 GOOS="windows" GOARCH="386" go build -o ../run.386.exe
